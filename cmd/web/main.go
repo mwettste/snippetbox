af9dd8a2dcde941058eb6296e2c0b3f956afcb68
Marco <marco.wettstein@gmail.com>
2022-03-26 07:52:44 +0100

    included static files in template added http middleware to disallow directory listing

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"log"
 	"net/http"
+
+	"github.com/mwettste/snippetbox/pkg/fsmiddleware"
 )
 
 func main() {
@@ -10,6 +12,9 @@ func main() {
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet", showSnippet)
 	mux.HandleFunc("/snippet/create", createSnippet)
+
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	mux.Handle("/static/", http.StripPrefix("/static", fsmiddleware.DisallowDirListing(fileServer)))
 
 	log.Println("Starting server on :4000")
 	err := http.ListenAndServe("localhost:4000", mux)
---
+++ pkg/fsmiddleware/fsmiddleware.go
@@ -0,0 +1,17 @@
+package fsmiddleware
+
+import (
+	"net/http"
+	"strings"
+)
+
+func DisallowDirListing(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if strings.HasSuffix(r.URL.Path, "/") {
+			http.NotFound(w, r)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
