75b48d5fe87346e264019d5eaa38d0f83786870b
Marco <marco.wettstein@gmail.com>
2022-04-17 18:10:56 +0200

    added helpers to spin up a  new integration test server

--- cmd/web/handlers_test.go
+++ cmd/web/handlers_test.go
@@ -1,37 +1,21 @@
 package main
 
 import (
-	"io"
-	"log"
 	"net/http"
-	"net/http/httptest"
 	"testing"
 )
 
 func TestPing(t *testing.T) {
-	app := &application{
-		errorLog: log.New(io.Discard, "", 0),
-		infoLog:  log.New(io.Discard, "", 0),
-	}
-
-	ts := httptest.NewTLSServer(app.routes())
+	app := newTestApplication(t)
+	ts := newTestServer(t, app.routes())
 	defer ts.Close()
 
-	// responseRecorder
-	rs, err := ts.Client().Get(ts.URL + "/ping")
-	if err != nil {
-		t.Fatal(err)
+	statusCode, _, body := ts.get(t, "/ping")
+
+	if statusCode != http.StatusOK {
+		t.Errorf("want %d; got %d", http.StatusOK, statusCode)
 	}
 
-	if rs.StatusCode != http.StatusOK {
-		t.Errorf("want %d; got %d", http.StatusOK, rs.StatusCode)
-	}
-
-	defer rs.Body.Close()
-	body, err := io.ReadAll(rs.Body)
-	if err != nil {
-		t.Fatal(err)
-	}
 	if string(body) != "OK" {
 		t.Errorf("want body to equal %q", "OK")
 	}
