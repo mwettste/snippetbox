05fef77dce67e8d57b9ae2d2a9ea13a2805ab863
Marco <marco.wettstein@gmail.com>
2022-04-17 19:01:08 +0200

    mocking models and writing an integration test for /snippets/get

--- cmd/web/handlers_test.go
+++ cmd/web/handlers_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"net/http"
 	"testing"
 )
@@ -18,5 +19,40 @@ func TestPing(t *testing.T) {
 
 	if string(body) != "OK" {
 		t.Errorf("want body to equal %q", "OK")
+	}
+}
+
+func TestGetSnippet(t *testing.T) {
+	app := newTestApplication(t)
+	ts := newTestServer(t, app.routes())
+	defer ts.Close()
+
+	tests := []struct {
+		name         string
+		urlPath      string
+		expectedCode int
+		expectedBody []byte
+	}{
+		{"Valid ID", "/snippet/1", http.StatusOK, []byte("An old silent pond...")},
+		{"Non-existent ID", "/snippet/2", http.StatusNotFound, nil},
+		{"Negative ID", "/snippet/-1", http.StatusNotFound, nil},
+		{"Decimal ID", "/snippet/1.23", http.StatusNotFound, nil},
+		{"String ID", "/snippet/foo", http.StatusNotFound, nil},
+		{"Empty ID", "/snippet/", http.StatusNotFound, nil},
+		{"Trailing slash", "/snippet/1/", http.StatusNotFound, nil},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			code, _, body := ts.get(t, test.urlPath)
+
+			if code != test.expectedCode {
+				t.Errorf("expected %d; got %d", test.expectedCode, code)
+			}
+
+			if !bytes.Contains(body, test.expectedBody) {
+				t.Errorf("expected body to contain %q", test.expectedBody)
+			}
+		})
 	}
 }
