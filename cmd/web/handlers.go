ab87ffcbe2b5bdfac59b57962fa70348475fff40
Marco <marco.wettstein@gmail.com>
2022-04-18 13:29:09 +0200

    implement change pw functionality

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -162,6 +162,50 @@ func (app *application) userProfile(writer http.ResponseWriter, r *http.Request)
 	app.render(writer, r, "profile.page.tmpl", &templateData{AuthenticatedUser: user})
 }
 
+func (app *application) changePasswordForm(writer http.ResponseWriter, r *http.Request) {
+	app.render(writer, r, "changepassword.page.tmpl", &templateData{
+		Form: forms.New(nil),
+	})
+}
+
+func (app *application) changePassword(writer http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+	if err != nil {
+		app.clientError(writer, http.StatusBadRequest)
+		return
+	}
+
+	form := forms.New(r.PostForm)
+	form.Required("currentPassword", "newPassword", "newPasswordConfirmation")
+	form.MinLength("newPassword", 10)
+	if form.Get("newPassword") != form.Get("newPasswordConfirmation") {
+		form.Errors.Add("newPasswordConfirmation", "Passwords do not match")
+	}
+
+	if !form.Valid() {
+		app.render(writer, r, "changepassword.page.tmpl", &templateData{
+			Form: form,
+		})
+		return
+	}
+
+	userID := app.session.GetInt(r, "authenticatedUserID")
+	err = app.users.ChangePassword(userID, form.Get("currentPassword"), form.Get("newPassword"))
+	if err != nil {
+		if errors.Is(err, models.ErrInvalidCredentials) {
+			form.Errors.Add("currentPassword", "Current password is incorrect")
+			app.render(writer, r, "changepassword.page.tmpl", &templateData{Form: form})
+		} else {
+			app.serverError(writer, err)
+		}
+
+		return
+	}
+
+	app.session.Put(r, "flash", "Your password has been updated!")
+	http.Redirect(writer, r, "/user/profile", http.StatusSeeOther)
+}
+
 func (app *application) about(writer http.ResponseWriter, r *http.Request) {
 	app.render(writer, r, "about.page.tmpl", &templateData{})
 }
--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -14,15 +14,21 @@ func (app *application) routes() http.Handler {
 
 	mux := pat.New()
 	mux.Get("/", dynamicMiddleware.ThenFunc(app.home))
+
+	// authenticated
 	mux.Get("/snippet/create", dynamicMiddleware.Append(app.requireAuthentication).ThenFunc(app.createSnippetForm))
 	mux.Post("/snippet/create", dynamicMiddleware.Append(app.requireAuthentication).ThenFunc(app.createSnippet))
+	mux.Post("/user/logout", dynamicMiddleware.Append(app.requireAuthentication).ThenFunc(app.logoutUser))
+	mux.Get("/user/profile", dynamicMiddleware.Append(app.requireAuthentication).ThenFunc(app.userProfile))
+	mux.Get("/user/change-password", dynamicMiddleware.Append(app.requireAuthentication).ThenFunc(app.changePasswordForm))
+	mux.Post("/user/change-password", dynamicMiddleware.Append(app.requireAuthentication).ThenFunc(app.changePassword))
+
+	// unauthenticated
 	mux.Get("/snippet/:id", dynamicMiddleware.ThenFunc(app.showSnippet))
 	mux.Get("/user/signup", dynamicMiddleware.ThenFunc(app.signupUserForm))
 	mux.Post("/user/signup", dynamicMiddleware.ThenFunc(app.signupUser))
 	mux.Get("/user/login", dynamicMiddleware.ThenFunc(app.loginUserForm))
 	mux.Post("/user/login", dynamicMiddleware.ThenFunc(app.loginUser))
-	mux.Post("/user/logout", dynamicMiddleware.Append(app.requireAuthentication).ThenFunc(app.logoutUser))
-	mux.Get("/user/profile", dynamicMiddleware.Append(app.requireAuthentication).ThenFunc(app.userProfile))
 	mux.Get("/about", dynamicMiddleware.ThenFunc(app.about))
 
 	mux.Get("/ping", http.HandlerFunc(ping))
